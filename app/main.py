038561d064a9d6638abe537ad9b353936e23da7e
PC <muoivo.051064@gmail.com>
2025-03-14 15:07:13 +0700

    fix cors

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from app.routes import user, crawl_image
+from app.routes import crawl_image
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Chỉ định origin được phép
+    allow_origins=["https://crawlclient.vercel.app/"],  # Chỉ định origin được phép
     allow_credentials=True,
     allow_methods=["*"],  # Cho phép tất cả phương thức (GET, POST, v.v.)
     allow_headers=["*"],  # Cho phép tất cả header
